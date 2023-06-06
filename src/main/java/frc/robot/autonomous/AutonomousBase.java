2b20c4278b982a7e52edd18ec48b4d5c16282ef8
A7ussein <ahmed.yah242@gmail.com>
2023-06-06 16:15:59 -0400

    Initial commit

---
+++ src/main/java/frc/robot/autonomous/AutonomousBase.java
@@ -0,0 +1,39 @@
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Components;
+import frc.robot.Constants;
+
+public abstract class AutonomousBase {
+    protected Components components;
+    protected double startTime;
+    protected double currentTime;
+    protected double targetRollerSpeed;
+
+    public AutonomousBase(Components components) {
+        this.components = components;
+    }
+
+    public void init() {
+        this.startTime = Timer.getFPGATimestamp();
+
+        switch (components.targetChooser.getSelected()) {
+            case Components.kLow:
+                targetRollerSpeed = Constants.autoConstants.shooting.lowTargetSpeed;
+                break;
+            case Components.kMid:
+                targetRollerSpeed = Constants.autoConstants.shooting.midTargetSpeed;
+                break;
+            default:
+                targetRollerSpeed = Constants.autoConstants.shooting.lowTargetSpeed;
+                break;
+        }
+        
+        components.leftEncoder.setPosition(0);
+        components.gyro.reset();
+    }
+
+    public void periodic() {
+        this.currentTime = Timer.getFPGATimestamp();
+    }
+}
---
+++ src/main/java/frc/robot/autonomous/OneAndHalf.java
@@ -0,0 +1,119 @@
+/*
+* this auto is going to deposit a cube on the low scoring area
+* drive forward 8.5 wheel rotations
+* check if the robot arm is backward
+*      if it is backward it will make it forward
+*      then it will check if there is a cube in the intake
+*      if there is no cube in the intake, it will run the intake motor until there is a cube in the intake
+*      then stop
+*/
+package frc.robot.autonomous;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import frc.robot.Components;
+
+
+public class OneAndHalf extends AutonomousBase {
+    private enum State{
+        firstCubeDeposit,
+        lowerArm,
+        driveForwardUntilCube,
+        raiseArm,
+        driveBackward,
+        secondCubeDeposit,
+        Finished;
+    }
+
+    private State currentState;
+    private final double encoder2inches = 1 / 8.46; // Unit Conversion
+
+    public OneAndHalf(Components components) {
+        super(components);
+        currentState = State.firstCubeDeposit;
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+
+        switch(currentState){
+            case firstCubeDeposit:
+                if(this.currentTime - this.startTime < 1){
+                    components.rollerMotor.set(ControlMode.PercentOutput, 0.5);
+                }else{
+                    components.rollerMotor.set(ControlMode.PercentOutput, 0);
+                    currentState = State.lowerArm;
+                }
+            break;
+            
+            case lowerArm:
+                if (components.frontLimitSensor.get() == false) {
+                    components.raisingMotor.set(ControlMode.PercentOutput, -0.5);
+                } else {
+                    components.raisingMotor.set(0);
+                    System.out.println("The following state is over: " + currentState);
+                    currentState = State.driveForwardUntilCube;
+                }
+                break;
+            case driveForwardUntilCube:
+                if(!components.cubeSensor.get()){
+                    System.out.println(components.leftEncoder.getPosition());
+                    if(components.leftEncoder.getPosition() > 92 ){
+                        components.drive.tankDrive(0, 0);
+                        components.rollerMotor.set(0);
+                        System.out.println("The following state is over: " + currentState);
+                        currentState = State.Finished;
+                        break;
+                    }else{
+                    components.drive.tankDrive(0.5, 0.5);
+                    components.rollerMotor.set(ControlMode.PercentOutput, -0.7);
+                    }
+                }else{
+                    components.rollerMotor.set(ControlMode.PercentOutput, 0);
+                    components.drive.tankDrive(0, 0);
+                    currentState = State.raiseArm;
+                }
+            break;
+
+            case raiseArm:
+            if (!components.backLimitSensor.get()) {
+                components.raisingMotor.set(ControlMode.PercentOutput, 0.45);
+                System.out.println("The following state is over: " + currentState);
+            }else{
+                    components.raisingMotor.set(ControlMode.PercentOutput, 0);
+                    currentState = State.Finished;
+                }
+            break;
+
+            case driveBackward:
+                if((Math.abs(components.leftEncoder.getPosition())) < 3){
+                    components.drive.tankDrive(0, 0);
+                    currentState = State.secondCubeDeposit;
+                }else{
+                    components.drive.tankDrive(-0.5, -0.5);
+                }
+            break;
+                
+            case secondCubeDeposit:
+                if(components.cubeSensor.get()){
+                    components.rollerMotor.set(ControlMode.PercentOutput, .7);
+                }else{
+                    currentState = State.Finished;
+                }
+            break;
+
+            case Finished:
+                components.drive.tankDrive(0, 0);
+                components.rollerMotor.set(ControlMode.PercentOutput, 0);
+                components.raisingMotor.set(ControlMode.PercentOutput, 0);
+            break;
+
+            default:
+                break;
+        }
+
+
+
+    }
+}
